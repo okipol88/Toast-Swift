8bb626072cce81573e5fd05f79278421e79798d4
Błażej Szajrych <blazej.szajrych@gmail.com>
2016-02-27 16:51:29 +0100

    Added framework library.

---
+++ framework/ToastSwift/ToastSwift/ToastSwift.h
@@ -0,0 +1,19 @@
+//
+//  ToastSwift.h
+//  ToastSwift
+//
+//  Created by Jacek Knaflewski on 27.02.2016.
+//  Copyright © 2016 ITGenerator. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for ToastSwift.
+FOUNDATION_EXPORT double ToastSwiftVersionNumber;
+
+//! Project version string for ToastSwift.
+FOUNDATION_EXPORT const unsigned char ToastSwiftVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ToastSwift/PublicHeader.h>
+
+
